9f99efdf85cda6dd0e401c585916650ac74f8ee9
aniruddh.fataniya <fataniya.ag@gmail.com>
2022-10-27 23:04:57 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateTemplateComponent } from './create-template/create-template.component';
+import { EmailHistoryComponent } from './email-history/email-history.component';
+import { HomeComponent } from './home/home.component';
+import { ListRegisteredEmailsComponent } from './list-registered-emails/list-registered-emails.component';
+import { RegisterEmailIdentityComponent } from './register-email-identity/register-email-identity.component';
+import { SendBulkEmailwithTemplateComponent } from './send-bulk-emailwith-template/send-bulk-emailwith-template.component';
+import { TemplateListComponent } from './template-list/template-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "send-bulk-email",
+    component: SendBulkEmailwithTemplateComponent
+  },
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path:"template-list",
+    component: TemplateListComponent
+  },
+  {
+    path:"email-history",
+    component:EmailHistoryComponent
+  },
+  {
+    path:"identity-list",
+    component:ListRegisteredEmailsComponent
+  },
+  {
+    path:"register-identity",
+    component:RegisterEmailIdentityComponent
+  },
+  {
+    path:"create-template",
+    component:CreateTemplateComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SendBulkEmailwithTemplateComponent } from './send-bulk-emailwith-template/send-bulk-emailwith-template.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule} from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TemplateListComponent } from './template-list/template-list.component';
+import { EmailHistoryComponent } from './email-history/email-history.component';
+import { ListRegisteredEmailsComponent } from './list-registered-emails/list-registered-emails.component';
+import { RegisterEmailIdentityComponent } from './register-email-identity/register-email-identity.component';
+import { CreateTemplateComponent } from './create-template/create-template.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { SanitizeHtmlPipe } from './sanitize-html-pipe';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SendBulkEmailwithTemplateComponent,
+    NavigationComponent,
+    HomeComponent,
+    TemplateListComponent,
+    EmailHistoryComponent,
+    ListRegisteredEmailsComponent,
+    RegisterEmailIdentityComponent,
+    CreateTemplateComponent,
+    SidebarComponent,
+    SanitizeHtmlPipe
+    
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
